95c8fbe10706eb89c04637d86b579939d73fa438
HassenDAI <41192176+HassenDAI-QA@users.noreply.github.com>
2025-07-23 16:48:06 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -5,20 +5,21 @@ pip install -r requirements.txt || exit /b 1
 echo Lancement des tests...
 robot -d results tests || exit /b 2
 
-echo Tests terminés.
-pause
+echo Envoi du rapport par email...
+powershell -Command "& {
+    $EmailFrom = 'hassen.dai@waialys-group.com'
+    $EmailTo = 'hassendai.info@gmail.com'
+    $Subject = 'Résultats des tests RobotFramework'
+    $Body = 'Veuvez trouver ci-joint les résultats des tests'
+    $SMTPServer = 'smtp.gmail.com'
+    $SMTPPort = '587'
+    $Password = 'omdu ynke qxit xkkr'
+    $SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+    $Credential = New-Object System.Management.Automation.PSCredential ($EmailFrom, $SecurePassword)
+    
+    Send-MailMessage -From $EmailFrom -To $EmailTo -Subject $Subject -Body $Body `
+        -SmtpServer $SMTPServer -Port $SMTPPort -UseSsl -Credential $Credential `
+        -Attachments 'results\report.html', 'results\log.html' -BodyAsHtml -ErrorAction Stop
+}" || exit /b 3
 
-$EmailFrom = "hassen.dai@waialys-group.com"
-$EmailTo = "hassendai.info@gmail.com"
-$Subject = "Statut des Tests"
-$Body = "Résultat des tests Robot Framework"
-$SMTPServer = "smtp.gmail.com"
-$SMTPPort = "587"
-$Password = "votre-mot-de-passe-d-application"  # Les 16 caractères
-
-$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
-$Credential = New-Object System.Management.Automation.PSCredential ($EmailFrom, $SecurePassword)
-
-Send-MailMessage -From $EmailFrom -To $EmailTo -Subject $Subject -Body $Body `
--SmtpServer $SMTPServer -Port $SMTPPort -UseSsl -Credential $Credential `
--ErrorAction Stop
+exit /b 0
