a8caefb42a54f5fbe010b067388d0d4ddc6b7da2
HassenDAI <41192176+HassenDAI-QA@users.noreply.github.com>
2025-07-23 16:58:14 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,18 @@
 @echo off
-echo Installation des dépendances...
-pip install -r requirements.txt || exit /b 1
-
-echo Lancement des tests...
-robot -d results tests || exit /b 2
-
 echo Envoi du rapport par email...
-
+powershell -Command "try {
+    $cred = New-Object System.Management.Automation.PSCredential(
+        'hassen.dai@waialys-group.com',
+        (ConvertTo-SecureString 'omdu ynke qxit xkkr' -AsPlainText -Force)
+    )
+    Send-MailMessage -From 'hassen.dai@waialys-group.com' -To 'hassendai.info@gmail.com' `
+        -Subject 'Résultats des tests RobotFramework' `
+        -Body 'Veuvez trouver ci-joint les résultats des tests' `
+        -SmtpServer 'smtp.gmail.com' -Port 587 -UseSsl `
+        -Credential $cred -Attachments 'results\report.html', 'results\log.html' `
+        -BodyAsHtml -ErrorAction Stop
+    exit 0
+} catch {
+    Write-Output $_
+    exit 3
+}"
